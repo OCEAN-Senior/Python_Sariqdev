3b76a7db6e2ba9a77f6813de6b5a70e35b170d6a
Ocean-senior <Kuhinur090999@gmail.com>
2021-08-23 22:28:23 +0500

    yaxshi

--- 11_dars/11_Amalyot.py
+++ 11_dars/11_Amalyot.py
@@ -54,19 +54,83 @@
 # "Mahsulot do'konimizda bor" aks holda,
 #  "Mahsulot do'konimizda yo'q" degan xabarlarni chiqaring.
 
-mahsulotlar = ['banan', 'anor', 'olma', 'shovurma', 'non', 'salat', 'un', 'kalbasa', 'yorma', 'dulana']
-savat = []
-print("Savatga kamida 5 ta mahsulot kiriting!")
-mahsulot_kiritish = int(input("Necha xil mahsulot harid qilmoqchisiz!? >> "))
-i = 1
-if mahsulot_kiritish > 4:
-    for mahsulot in range(1, mahsulot_kiritish + 1):
-        savat.append(str(input(f"{mahsulot} - mahsulot kiriting! >> ").lower()))
+# mahsulotlar = ['banan', 'anor', 'olma', 'shovurma', 'non', 'salat', 'un', 'kalbasa', 'yorma', 'dulana']
+# savat = []
+# print("Savatga kamida 5 ta mahsulot kiriting!")
+# mahsulot_kiritish = int(input("Necha xil mahsulot harid qilmoqchisiz!? >> "))
+# i = 1
+# if mahsulot_kiritish > 4:
+#     for mahsulot in range(1, mahsulot_kiritish + 1):
+#         savat.append(str(input(f"{mahsulot} - mahsulot kiriting! >> ").lower()))
 
-    for xarid in savat:
-        if xarid in mahsulotlar:
-            print(f"Bizda {xarid.title()} bor!")
-        else:
-            print(f"Bizda {xarid.title()} yo'q! ")
-else:
-    print(f"Siz kiritgan {mahsulot_kiritish} kichik 5 dan! \nQaytaddan kiriting!")
+#     for xarid in savat:
+#         if xarid in mahsulotlar:
+#             print(f"Bizda {xarid.title()} bor!")
+#         else:
+#             print(f"Bizda {xarid.title()} yo'q! ")
+# else:
+#     print(f"Siz kiritgan {mahsulot_kiritish} kichik 5 dan! \nQaytaddan kiriting!")
+
+#==================================================================================================================
+
+# Yuqoridagi dasturni quyidagicha o'zgartiring: foydalanuvchidan 5 ta mahsulot kiritishni so'rang. 
+# Foydalanuvchi so'ragan va do'konda bor mahsulotlarni yang, bor_mahsulotlar degan ro'yxatga, 
+# do'konda yo'q mahsulotlarni esa mavjud_emas degan ro'yxatga qo'shing.  Agar mavjud_emas 
+# ro'yxati bo'sh bo'lsa, "Siz so'ragan barcha mahsulotlar do'konimizda bor" degan xabarni, aks holda esa 
+# "Quyidagi mahsulotlar do'konimizda yo'q: ....." degan xabarni chiqaring.
+
+# mahsulotlar = ['banan', 'anor', 'olma', 'shovurma', 'non', 'salat', 'un', 'kalbasa', 'yorma', 'dulana']
+# savat = []
+# bor_mahsulotlar = []
+# mavjud_emas = []
+# print("Savatga kamida 5 ta mahsulot kiriting!")
+# mahsulot_kiritish = int(input("Necha xil mahsulot harid qilmoqchisiz!? >> "))
+# i = 1
+# if mahsulot_kiritish > 4:
+#     for mahsulot in range(1, mahsulot_kiritish + 1):
+#         savat.append(str(input(f"{mahsulot} - mahsulot kiriting! >> ").lower()))
+
+#     for xarid in savat:
+#         if xarid in mahsulotlar:
+#             #print(f"Bizda {xarid.title()} bor!")
+#             bor_mahsulotlar.append(xarid)
+#         else:
+#             # print(f"Bizda {xarid.title()} yo'q! ")
+#             mavjud_emas.append(xarid)
+# else:
+#     print(f"Siz kiritgan {mahsulot_kiritish} kichik 5 dan! \nQaytaddan kiriting!")
+
+# if mavjud_emas == 0:
+#     print("Xayr!!!")
+# else:
+#     print("Do'konimizda quyidagi mahsulot mavjud emas!")
+#     for emas in mavjud_emas:
+#         print(emas.title())
+#==================================================================================================================
+
+# foydalanuvchilar degan ro'yxat tuzing, va kamida 5 ta login qo'shing. 
+# Foydalanuvchidan yangi login tanlashni so'rang va foydalanuvchi kiritgan 
+# loginni foydalanuvchilar degan ro'yxatning tarkibi bilan solishtiring. 
+# Agar ro'yxatda bunday login mavjud bo'lsa, "Login band, yangi login tanlang!" 
+# aks holda "Xush kelibsiz, foydalanuvchi!" xabarini chiqaring.
+
+# foydalanuvchilar = ['salom', 'xayr', 'nima', 'qani', 'moni']
+# yangi_login = str(input("Yangi login tanlang! >> ").lower())
+
+# if yangi_login in foydalanuvchilar:
+#     print("\n Login band, yangi login tanlang! \n")
+# else:
+#     print("\n Xush kelibsiz! \n Xurmatli Admin! \n")
+
+#==================================================================================================================
+
+# Foydalanuvchidan biror butun son kiritishni so'rang. 
+# Foydalanuvchi kiritgan sonni 2 da 10 gacha 
+# bo'lgan sonlardan qay biriga qoldiqsiz 
+# bo'linishini konsolga chiqaring. 
+
+son = int(input("Istalgan butun son kiriting: "))
+
+for n in range(2,11):
+    if not (son%n):
+        print(f"{son} soni {n} ga qoldiqsiz bo'linadi")
